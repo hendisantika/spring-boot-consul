87a15e8861c11ffe34f01b81ee1faf396d7ed8fd
hendisantika <hendisantika@yahoo.co.id>
2025-01-15 07:13:34 +0700

    Add DynamoDbConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootconsul/config/DynamoDbConfig.java
@@ -0,0 +1,48 @@
+package id.my.hendisantika.springbootconsul.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-consul
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/01/25
+ * Time: 07.13
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class DynamoDbConfig {
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(buildAmazonDynamoDB());
+    }
+
+    private AmazonDynamoDB buildAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                "dynamodb.region.amazonaws.com", "region"
+                        )
+                )
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(
+                                        "ACCESS_KEY_ID",
+                                        "ACCESS_KEY_VALUE"
+                                )
+                        )
+                )
+                .build();
+    }
+}
