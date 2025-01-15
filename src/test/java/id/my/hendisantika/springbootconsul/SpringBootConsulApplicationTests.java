ce3de3cecf86ccb3e9e1e7d712e2af905164c5f1
hendisantika <hendisantika@yahoo.co.id>
2025-01-15 07:08:37 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/springbootconsul/SpringBootConsulApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootconsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConsulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
