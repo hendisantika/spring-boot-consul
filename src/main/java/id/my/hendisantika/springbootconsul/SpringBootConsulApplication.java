f33652772f5a1a4e5a215a1f6889abfcae12e03d
hendisantika <hendisantika@yahoo.co.id>
2025-01-15 07:18:58 +0700

    Add EnableConfigurationProperties n EnableDiscoveryClient🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootconsul/SpringBootConsulApplication.java
+++ src/main/java/id/my/hendisantika/springbootconsul/SpringBootConsulApplication.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.springbootconsul;
 
+import id.my.hendisantika.springbootconsul.config.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties(value = ApplicationConfig.class)
 public class SpringBootConsulApplication {
 
     public static void main(String[] args) {
