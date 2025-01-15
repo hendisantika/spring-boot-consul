3f06530b8852cb25f0e63f52d49d527260467e84
hendisantika <hendisantika@yahoo.co.id>
2025-01-15 07:15:21 +0700

    Add EmployeeRepository🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootconsul/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.springbootconsul.repository;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-consul
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/01/25
+ * Time: 07.14
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+@RequiredArgsConstructor
+public class EmployeeRepository {
+    private final DynamoDBMapper dynamoDBMapper;
+}
