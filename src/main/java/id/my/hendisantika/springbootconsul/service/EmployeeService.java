0cd13a6e62a724868dc1090be1bc6087e330e7f3
hendisantika <hendisantika@yahoo.co.id>
2025-01-15 07:17:22 +0700

    Add EmployeeService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootconsul/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.springbootconsul.service;
+
+import id.my.hendisantika.springbootconsul.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-consul
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/01/25
+ * Time: 07.16
+ * To change this template use File | Settings | File Templates.
+ */
+@Service("employeeService")
+@RequiredArgsConstructor
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository
+}
