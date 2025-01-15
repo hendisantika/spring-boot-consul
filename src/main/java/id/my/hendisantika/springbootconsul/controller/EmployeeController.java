bb7b76c383f00a16520a327f9baa389fa0d6184f
hendisantika <hendisantika@yahoo.co.id>
2025-01-15 07:20:53 +0700

    Add getEmployee in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootconsul/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/springbootconsul/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +41,10 @@ public class EmployeeController {
         log.info(applicationConfig.getUserName());
         log.info(applicationConfig.getPassword());
         return employeeService.save(employeeObj);
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@PathVariable("id") String employeeId) {
+        return employeeService.getEmployeeId(employeeId);
     }
 }
