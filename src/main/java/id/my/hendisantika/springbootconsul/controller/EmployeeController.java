79b94e01c647033be77f6439ff3f0a85adf1ba71
hendisantika <hendisantika@yahoo.co.id>
2025-01-15 07:20:33 +0700

    Add saveEmployee in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootconsul/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/springbootconsul/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package id.my.hendisantika.springbootconsul.controller;
 
 import id.my.hendisantika.springbootconsul.config.ApplicationConfig;
+import id.my.hendisantika.springbootconsul.entity.Employee;
 import id.my.hendisantika.springbootconsul.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,14 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 07.19
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @RestController
 @RefreshScope
 @EnableDiscoveryClient
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/employee")
 @RequiredArgsConstructor
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
     private final ApplicationConfig applicationConfig;
+
+    @PostMapping
+    public Employee saveEmployee(@RequestBody Employee employeeObj) {
+        log.info(applicationConfig.getUserName());
+        log.info(applicationConfig.getPassword());
+        return employeeService.save(employeeObj);
+    }
 }
